467ac85bb59b5e7ab137128984b77c7b582c4582
tet19 <tet19@pitt.edu>
2016-10-11 21:58:28 -0400

    adding a boatload of things

---
+++ js/js_stuff.js
@@ -0,0 +1,16 @@
+$(document).on('ready', function() {
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+  $('.one-time').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
+});
